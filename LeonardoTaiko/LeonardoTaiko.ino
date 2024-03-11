c64088f9acedd375024c68e0d57752bbe8a06188
RyuMiya <lty2008one@163.com>
2024-03-11 16:28:16 +0800

    Add parallel Key pressing

--- LeonardoTaiko/LeonardoTaiko.ino
+++ LeonardoTaiko/LeonardoTaiko.ino
@@ -1,18 +1,29 @@
-#include <Keyboard.h>
-#include <KeyboardLayout.h>
-#include <NintendoSwitchControlLibrary.h>
 #include <EEPROM.h>
+#include "pressKey.h"
 
 const float min_threshold = 75;  // The minimum rate on triggering a input
 const int cd_length = 20; //Buffer loop times.
 const float k_decay = 0.99; //decay speed on the dynamite threshold.
 const float k_increase = 0.7;  //Dynamite threshold range.
+
 const int outputDuration_pc = 7; // For PC. How long a key should be pressed when triggering a input.
 const int outputDuration_ns = 35; // For NS. How long a key should be pressed when triggering a input.
 
+// {LK, LD, RD, RK}
+const KeyUnion NS_LEFT_KATSU[4]  = {{Button::ZL, NS_BTN, NS_BTN_DUR, 0, false}, {Button::L, NS_BTN, NS_BTN_DUR, 0, false}, {Hat::UP, NS_HAT, NS_HAT_DUR, 0, false}, {Hat::LEFT, NS_HAT, NS_HAT_DUR, 0, false}};
+const KeyUnion NS_LEFT_DON[3]    = {{Button::LCLICK, NS_BTN, NS_BTN_DUR, 0, false}, {Hat::RIGHT, NS_HAT, NS_HAT_DUR, 0, false}, {Hat::DOWN, NS_HAT, NS_HAT_DUR, 0, false}};
+const KeyUnion NS_RIGHT_DON[3]   = {{Button::RCLICK, NS_BTN, NS_BTN_DUR, 0, false}, {Button::Y, NS_BTN, NS_BTN_DUR, 0, false}, {Button::B, NS_BTN, NS_BTN_DUR, 0, false}};
+const KeyUnion NS_RIGHT_KATSU[4] = {{Button::ZR, NS_BTN, NS_BTN_DUR, 0, false}, {Button::R, NS_BTN, NS_BTN_DUR, 0, false}, {Button::X, NS_BTN, NS_BTN_DUR, 0, false}, {Button::A, NS_BTN, NS_BTN_DUR, 0, false}};
+const KeyUnion PC_LEFT_KATSU[1]  = {{'d', PC_BTN, PC_BTN_DUR, 0, false}};
+const KeyUnion PC_LEFT_DON[1]    = {{'f', PC_BTN, PC_BTN_DUR, 0, false}};
+const KeyUnion PC_RIGHT_DON[1]   = {{'j', PC_BTN, PC_BTN_DUR, 0, false}};
+const KeyUnion PC_RIGHT_KATSU[1] = {{'k', PC_BTN, PC_BTN_DUR, 0, false}};
+const int NS_SIZE[4] = {sizeof(NS_LEFT_KATSU) / sizeof(KeyUnion), sizeof(NS_LEFT_DON) / sizeof(KeyUnion), sizeof(NS_RIGHT_DON) / sizeof(KeyUnion), sizeof(NS_RIGHT_KATSU) / sizeof(KeyUnion)};
+const int PC_SIZE[4] = {sizeof(PC_LEFT_KATSU) / sizeof(KeyUnion), sizeof(PC_LEFT_DON) / sizeof(KeyUnion), sizeof(PC_RIGHT_DON) / sizeof(KeyUnion), sizeof(PC_RIGHT_KATSU) / sizeof(KeyUnion)};
+
 //{A3, A0, A1, A2}
-const uint16_t keymapping_ns[4] = {Button::A, Hat::UP, Hat::DOWN, Button::B};
-const int keymapping[4] = {'k','d','f','j'};
+// const uint16_t keymapping_ns[4] = {Button::A, Hat::UP, Hat::DOWN, Button::B};
+// const int keymapping[4] = {'k','d','f','j'};
 const uint16_t keymapping_ns_extend[] = {Button::PLUS, Hat::RIGHT};
 
 int mode; //0 for keyboard, 1 for switch
@@ -28,31 +39,34 @@ void setup() {
   pinMode(1, INPUT_PULLUP);
   int pc_status = digitalRead(0);
   int ns_status = digitalRead(1);
+
+  // 初始化读取按键电平
   if (ns_status == LOW && pc_status == HIGH){
-    mode = 1;
+    mode = 1;     // 按下NS按键，初始化为NS模式
     outputDuration = outputDuration_ns; 
-    EEPROM.write(0, 1);
+    EEPROM.write(0, 1);   // 写入EEPROM
     }
   if (pc_status == LOW && ns_status == HIGH){
-    mode = 0;
+    mode = 0;     // 按下PC按键，初始化为PC模式
     outputDuration = outputDuration_pc; 
-    EEPROM.write(0, 0);
+    EEPROM.write(0, 0);   // 写入EEPROM
     }
   else{
+    // 没有按任何按键，从EEPROM中读取之前的控制状态
     mode = EEPROM.read(0);
-    if(mode == 0){
+    if(mode == 0){    // 默认值与PC状态
       outputDuration = outputDuration_pc;
       }
-    if(mode == 1){
+    if(mode == 1){    // NS状态
       outputDuration = outputDuration_ns;
       }
     }
 
   if(mode == 1){  
-    pushButton(Button::A, 500, 3); //initialize on switch
+    pushButton(Button::A, 500, 3); // 初始化时按键自动连接到SWITCH
     }
   if(mode == 0){
-    Keyboard.begin();
+    Keyboard.begin();              // 初始化启动按键输入
   }
 }
 
@@ -60,6 +74,7 @@ void loop() {
 
 //  analogMonitor();
   extendKey();
+  release();
     
   bool output = false;
   int sensorValue[] = {analogRead(A0),analogRead(A3),analogRead(A1),analogRead(A2)};
@@ -88,30 +103,27 @@ void loop() {
       }
     }
     threshold = temp*k_increase;
-    key = count%4;
-    Serial.println(temp);
-    Serial.println(threshold);
-    Serial.println(key);
-    if(mode == 0){
-      Keyboard.press(keymapping[key]);
-      delay(outputDuration);
-      Keyboard.releaseAll();
+    key = (count + 1) % 4;          // 处理后，变成  0:LK  1:LD  2:RD  3:RK
+    bool pressed = false;
+    if (mode == 0) {      // PC模式输出
+      switch (key) {
+        case 0: pressed = press(PC_SIZE[key], PC_LEFT_KATSU); break;
+        case 1: pressed = press(PC_SIZE[key], PC_LEFT_DON); break;
+        case 2: pressed = press(PC_SIZE[key], PC_RIGHT_DON); break;
+        case 3: pressed = press(PC_SIZE[key], PC_RIGHT_KATSU); break;
+      }
+    } else {              // NS模式输出
+      switch (key) {
+        case 0: pressed = press(NS_SIZE[key], NS_LEFT_KATSU); break;
+        case 1: pressed = press(NS_SIZE[key], NS_LEFT_DON); break;
+        case 2: pressed = press(NS_SIZE[key], NS_RIGHT_DON); break;
+        case 3: pressed = press(NS_SIZE[key], NS_RIGHT_KATSU); break;
+      }
     }
-    else if(mode == 1){
-      if( key ==1 || key == 2 ){
-        SwitchControlLibrary().pressHatButton(keymapping_ns[key]);
-        SwitchControlLibrary().sendReport();
-        delay(outputDuration);
-        SwitchControlLibrary().releaseHatButton();
-        SwitchControlLibrary().sendReport();
-      }
-      else{
-        SwitchControlLibrary().pressButton(keymapping_ns[key]);
-        SwitchControlLibrary().sendReport();
-        delay(outputDuration);
-        SwitchControlLibrary().releaseButton(keymapping_ns[key]);
-        SwitchControlLibrary().sendReport();
-      }
+    if (pressed) {
+      Serial.println(temp);
+      Serial.println(threshold);
+      Serial.println(key);
     }
   }
   if(threshold < min_threshold){
@@ -175,3 +187,11 @@ void extendKey(){
       }
   }
 }
+
+
+
+
+
+
+
+
---
+++ LeonardoTaiko/pressKey.h
@@ -0,0 +1,151 @@
+#include <Keyboard.h>
+#include <KeyboardLayout.h>
+#include <NintendoSwitchControlLibrary.h>
+#include <ArrayList.h>
+
+#define PC_BTN 0
+#define NS_BTN 1
+#define NS_HAT 2
+
+#define PC_BTN_DUR 7
+#define NS_BTN_DUR 25
+#define NS_HAT_DUR 35
+
+struct KeyUnion {
+  uint16_t key;
+  uint8_t mode;
+  unsigned long delay;
+  unsigned long unlock;
+  bool pressed;
+};
+
+struct Node {
+  KeyUnion* key;
+  Node* next;
+};
+
+uint8_t current_hat = Hat::NEUTRAL;
+Node* pressed = nullptr;
+
+bool press(int size, KeyUnion* keys);           // 选择并按下按键
+bool press0(KeyUnion* key);                     // 执行按下按键操作
+void release();                                 // 释放过期按键
+void preppend(KeyUnion* key);                   // 追加按键到待过期列表
+uint8_t hat_add(uint8_t cur, uint8_t add);      // 方向键相加
+uint8_t hat_sub(uint8_t cur, uint8_t sub);      // 方向键相减
+
+void release() {
+  if (pressed == nullptr) return;
+  unsigned long current = millis();
+  Node** lastPtr = &pressed;
+  Node* select = pressed;
+  bool nsTriggered = false;
+  while (select != nullptr) {
+    if (select->key->unlock <= current) {
+      switch (select->key->mode) {
+        case PC_BTN: {
+          Keyboard.release((uint8_t) select->key->key);
+          select->key->pressed = false;
+          *lastPtr = select->next;
+        }; break;
+        case NS_BTN: {
+          SwitchControlLibrary().releaseButton(select->key->key);
+          select->key->pressed = false;
+          *lastPtr = select->next;
+          nsTriggered = true;
+        }; break;
+        case NS_HAT: {
+          uint8_t result = hat_sub(current_hat, (uint8_t) (select->key->key));
+          if (result == Hat::NEUTRAL) {
+            SwitchControlLibrary().releaseHatButton();
+          } else {
+            SwitchControlLibrary().pressHatButton(result);
+          }
+          select->key->pressed = false;
+          *lastPtr = select->next;
+          nsTriggered = true;
+        }; break;
+      }
+    }
+    select = select->next;
+    lastPtr = &(select->next);
+  }
+  if (nsTriggered) SwitchControlLibrary().sendReport();
+}
+
+bool press(int size, KeyUnion* keys) {
+  for (int i = 0; i < size; i++) {
+    bool pressed = press0(keys + i);
+    if (pressed) return true;
+  }
+  return false;
+}
+
+bool press0(KeyUnion* key) {
+  if (key == nullptr) return false;
+  if (key->pressed == true) return false;
+  switch (key->mode) {
+    case PC_BTN: {
+      Keyboard.press((uint8_t) (key->key));
+      unsigned long time = millis();
+      key->unlock = time + key->delay;
+      preppend(key);
+      return true;
+    };
+    case NS_BTN: {
+      SwitchControlLibrary().pressButton(key->key);
+      SwitchControlLibrary().sendReport();
+      unsigned long time = millis();
+      key->unlock = time + key->delay;
+      preppend(key);
+      return true;
+    };
+    case NS_HAT: {
+      uint8_t result = hat_add(current_hat, (uint8_t) (key->key));
+      if (result != current_hat) {
+        current_hat = result;
+        SwitchControlLibrary().pressHatButton(result);
+        SwitchControlLibrary().sendReport();
+        unsigned long time = millis();
+        key->unlock = time + key->delay;
+        preppend(key);
+        return true;
+      } else return false;
+    };
+  }
+  return false;
+}
+
+void preppend(KeyUnion* key) {
+  Node node = {key, pressed};
+  pressed = &node;
+}
+
+// 00000000 UP
+// 00000001 UP_RIGHT
+// 00000010 RIGHT
+// 00000011 DOWN_RIGHT
+// 00000100 DOWN
+// 00000101 DOWN_LEFT
+// 00000110 LEFT
+// 00000111 UP_LEFT
+// 00001000 NEUTRAL
+uint8_t hat_add(uint8_t cur, uint8_t add) {
+  if (cur == 0x08) return add;   // 如果当前为全部释放状态，则按下什么按键就是什么按键
+  if (cur & 0x01) return cur;   // 如果当前按键值为奇数，则必然已经是两个按键之和，返回无法按下
+  if (cur ^ 0x04 == add) return cur;  // 如果两个按键差值为4，则必然互斥
+  if (cur == 0x00 && add == 0x06) return 0x07;
+  if (cur == 0x06 && add == 0x00) return 0x07;
+  return (cur + add) >> 1;
+}
+
+uint8_t hat_sub(uint8_t cur, uint8_t sub) {
+  if (cur == 0x08) return cur;   // 如果当前为全部释放状态，则无法释放按键
+  if (cur == sub) return 0x08;  // 如果当前按键等于移除按键，则释放后按键为完全释放状态
+  if (cur & 0x01) {
+    if (cur - sub == 1 || sub - cur == 1) return (cur * 2 - sub) % 0x08;
+    if (cur == 0x07 && cur == 0x00) return 0x06;
+  }
+  return cur;     // 其余不能减的情况不减
+}
+
