db9796052f425a80013fc520b4d6e6eca180355a
judjdigj <67326129+judjdigj@users.noreply.github.com>
2024-08-12 16:36:01 +0800

    async io on switch test

--- LeonardoTaiko/LeonardoTaiko.ino
+++ LeonardoTaiko/LeonardoTaiko.ino
@@ -1,20 +1,32 @@
-#include <NintendoSwitchControlLibrary.h>
 #include <Keyboard.h>
 #include <EEPROM.h>
+#include <NintendoSwitchControlLibrary.h>
+
 
 
 const float min_threshold = 50;  // The minimum rate on triggering a input
 const int cd_length = 20; //Buffer loop times.
 const float k_decay = 0.99; //decay speed on the dynamite threshold.
 const float k_increase = 0.8;  //Dynamite threshold range.
-const int outputDuration_pc = 7; // For PC. How long a key should be pressed when triggering a input.
-const int outputDuration_ns = 22; // For NS. How long a key should be pressed when triggering a input.
+const int outputDuration = 7;
 
 //{A3, A0, A1, A2}
-const uint16_t keymapping_ns[4] = {Button::LCLICK, Button::ZL, Button::RCLICK, Button::ZR};
 const int keymapping[4] = {'f','d','j','k'};
 const uint16_t keymapping_ns_extend[] = {Button::PLUS, Hat::RIGHT};
 
+const uint16_t keymapping_ns_lka[3] = {Button::ZL, Button::L, Hat::UP};
+const uint16_t keymapping_ns_ldon[3] = {Button::LCLICK, Hat::DOWN, Hat::RIGHT};
+const uint16_t keymapping_ns_rdon[3] = {Button::RCLICK, Button::Y, Button::B};
+const uint16_t keymapping_ns_rka[3] = {Button::ZR, Button::R, Button::X};
+
+int keyStatus_lka = 0;
+int keyStatus_ldon = 0;
+int keyStatus_rdon = 0;
+int keyStatus_rka = 0;
+
+int previousMillis = 0;
+int previousMillis2 = 0;
+int previousMillis3 = 0;
 
 // 模式与计算按键与缓存
 int mode; //0 for keyboard, 1 for switch
@@ -23,16 +35,8 @@ const int buffer_size = cd_length*4;
 int buffer[buffer_size];
 int threshold = min_threshold;
 
-
-// 帧对齐
-// uint8_t dloop[3] = {33, 33, 34};                                  // 30帧对齐
-// uint8_t dloop[3] = {16, 17, 17};                                  // 60帧对齐
-// uint8_t dloop[3] = {8, 8, 9};                                     // 120帧对齐
-// uint8_t dloop[6] = {4, 4, 4, 4, 4, 5};                            // 240帧对齐
-// uint8_t dloop[12] = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3};         // 480帧对齐
-// uint8_t dsize = sizeof(dloop) / sizeof(uint8_t);
-// uint8_t loopc = 0;
-
+unsigned long currentMillis = 0;
+int switchMode = 0;
 
 void setup() {
 //  Serial.begin(9600);
@@ -69,8 +73,7 @@ void setup() {
 
 
 void loop() {
-  unsigned long begin = millis();
-//  analogMonitor();
+  currentMillis = millis();
   extendKey();
   bool output = false;
   int sensorValue[] = {analogRead(A0),analogRead(A3),analogRead(A1),analogRead(A2)};
@@ -79,6 +82,7 @@ void loop() {
       output = true;
     }
   }
+
   if (output){
     //Storage pin value into buffer.
     int j = 0;
@@ -99,61 +103,34 @@ void loop() {
     }
     threshold = temp*k_increase;
     key = count%4;
-//    Serial.println(temp);
-//    Serial.println(threshold);
-//    Serial.println(key);
+
     if(mode == 0){
       Keyboard.press(keymapping[key]);
-      delay(outputDuration_pc);
+      delay(outputDuration);
       Keyboard.releaseAll();
+      
     }
     else if(mode == 1){
-      SwitchControlLibrary().pressButton(keymapping_ns[key]);
-      SwitchControlLibrary().sendReport();
-      delay(outputDuration_ns);
-      SwitchControlLibrary().releaseButton(keymapping_ns[key]);
-      SwitchControlLibrary().sendReport();
+      switchMode = 1;
     }
+  }
+
+  if(switchMode = 1){
+    switchPress();
+  }
+  else{
+    previousMillis = 0;
+    previousMillis2 = 0;
+    previousMillis3 = 0;
   }
   
   if(threshold < min_threshold){
     threshold = min_threshold;
   } else if(threshold > min_threshold) {
     threshold = threshold*k_decay;
-    // Serial.println("DECAY");
-    // Serial.println(threshold); //Check decay, in order to set proper k_increase and k_decay.
   }
-  // unsigned long d = begin + dloop[loopc % dsize] - millis();
-  // while (d < 0) d += dloop[(++loopc) % dsize];
-  // loopc = (++loopc) % dsize;
-  // if (d > 0) delay(d);
 }
 
-/*
-void analogMonitor(){
-  bool output = false;
-  int sensorValue[] = {analogRead(A0),analogRead(A3),analogRead(A1),analogRead(A2)};
-  for (int i = 0; i <= 3; i++){
-    if (sensorValue[i] > threshold){
-      output = true;
-    }
-  }
-  
-  if (output) {
-    for (int j = 0; j < 100; j++) {
-      for (int pin = A0; pin <= A3; pin++) {
-        Serial.print("||");
-        Serial.print(analogRead(pin));
-        if (pin == A3) {
-          Serial.println("||");
-          Serial.println(j);
-          Serial.println("===========");
-        }
-      }
-    }
-  }
-}
-*/
 void extendKey(){
   if (mode == 1){
     if(digitalRead(0) == LOW || digitalRead(1) == LOW){
@@ -176,4 +153,52 @@ void extendKey(){
       }
     }
   }
+}
+
+
+
+
+void switchPress(){
+  if(keyStatus_ldon == 0){
+    if(previousMillis != 0 && currentMillis - previousMillis > 30){
+      SwitchControlLibrary().releaseButton(keymapping_ns_ldon[keyStatus_ldon]);
+      keyStatus_ldon = 0;
+      switchMode = 0;
+    }
+    else if(previousMillis == 0){
+      SwitchControlLibrary().pressButton(keymapping_ns_ldon[keyStatus_ldon]);
+      previousMillis = currentMillis;
+      keyStatus_ldon = 1;
+    }
+  }
+
+  else if(keyStatus_ldon = 1){
+    if(previousMillis2 != 0 && currentMillis - previousMillis2 > 30){
+      SwitchControlLibrary().releaseHatButton();
+      keyStatus_ldon = 0;
+    }
+    else{
+      SwitchControlLibrary().pressHatButton(keymapping_ns_ldon[keyStatus_ldon]);
+      if(previousMillis2 == 0){
+        previousMillis2 = currentMillis;
+      }
+      keyStatus_ldon = 2;
+    }
+  }
+
+  else if(keyStatus_ldon = 2){
+    if(previousMillis3 != 0 && currentMillis - previousMillis3 > 30){
+      SwitchControlLibrary().releaseHatButton();
+      keyStatus_ldon = 0;
+    }
+    else{
+      SwitchControlLibrary().pressHatButton(keymapping_ns_ldon[keyStatus_ldon]);
+      if(previousMillis3 == 0){
+        previousMillis3 = currentMillis;
+      }
+      keyStatus_ldon = 0;
+    }
+  }
+  SwitchControlLibrary().sendReport();
+
 }
