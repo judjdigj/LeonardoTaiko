d4dbea6716412b9b222c770d09c9d7383520980f
judjdigj <67326129+judjdigj@users.noreply.github.com>
2024-10-03 13:13:37 +0800

    Update buffer size

--- LeonardoTaiko/LeonardoTaiko.ino
+++ LeonardoTaiko/LeonardoTaiko.ino
@@ -38,15 +38,6 @@ unsigned long previousMillis2 = 0;
 unsigned long previousMillis3 = 0;
 unsigned long previousMillis4 = 0;
 
-// 帧对齐
-// uint8_t dloop[3] = {33, 33, 34};                                  // 30帧对齐
-// uint8_t dloop[3] = {16, 17, 17};                                  // 60帧对齐
-// uint8_t dloop[3] = {8, 8, 9};                                     // 120帧对齐
-// uint8_t dloop[6] = {4, 4, 4, 4, 4, 5};                            // 240帧对齐
-// uint8_t dloop[12] = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3};         // 480帧对齐
-// uint8_t dsize = sizeof(dloop) / sizeof(uint8_t);
-// uint8_t loopc = 0;
-
 
 void setup() {
   pinMode(0, INPUT_PULLUP);
